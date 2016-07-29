f0c517c3099ad8d834c411ecdd357b46d8551396
c7x43t <c7x43t@gmail.com>
2016-07-29 16:52:05 +0200

    Update _dom.js

--- _dom.js
+++ _dom.js
@@ -4,7 +4,7 @@ y.i=function(el){y._=y.doc.getElementById(el);return y};
 y.t=function(el,o){y._=y.doc.getElementsByTagName(el);o?y._=y._[0]:null;return y};
 y.c=function(el,o){y._=y.doc.getElementsByClassName(el);o?y._=y._[0]:null;return y};
 y.n=function(el,o){y._=y.doc.getElementsByName(el);o?y._=y._[0]:null;return y};
-y.append=function(){
-    this.tdoc.insertAdjacentHTML('afterbegin', '<div id="newChild">mark</div>');
+y.append=function(el){
+    y._.insertAdjacentHTML('afterbegin', el);
 }};
 _=new xx();
