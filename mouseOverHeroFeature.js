472ace09ebacab8f580cc51d1cbb58ccdae2eb0a
c7x43t <c7x43t@gmail.com>
2018-04-26 15:02:46 +0200

    Create mouseOverHeroFeature.js

---
+++ mouseOverHeroFeature.js
@@ -0,0 +1,36 @@
+$$(".hero-feature").on("mouseenter",e=>{
+function getImgSize(imgSrc, callback) {
+    var newImg = new Image();
+    newImg.onload = function () {
+        if (callback != undefined)
+            callback({width: newImg.width, height: newImg.height})
+    }
+    newImg.src = imgSrc;
+}
+//onhover:
+var e=$$(".hero-feature")[2];
+var img=$$.find(e,"img")[0];
+var src=img.src;
+var srcInfo=src.replace(/\/(thumb)\//g,"/info/");
+infoSize=getImgSize(srcInfo,size=>{
+	var imgWidth=$$.style(e).width;
+	imgWidth=Math.round(parseFloat(imgWidth));
+	var imgHeight=size.height*imgWidth/size.width;
+	var imgPreview=$$(".listing-img-preview")[0];
+	// set size
+	imgPreview.width=imgWidth;
+	imgPreview.height=imgHeight;
+	// calc pos
+	var ePos=e.getBoundingClientRect();
+	var imgPos=img.getBoundingClientRect();
+	var top=Math.round(imgPos.bottom-imgHeight+window.pageYOffset);
+	var left=Math.round(ePos.left);
+console.log(left);
+	//console.log({top:top,left:left,bottom:ePos.bottom,imgHeight:imgHeight});;
+	// set pos
+	imgPreview.style.top=top+"px";
+	imgPreview.style.left=left+"px";
+	imgPreview.style.display="block";
+});
+$$(".listing-img-preview")[0].getBoundingClientRect();
+})
