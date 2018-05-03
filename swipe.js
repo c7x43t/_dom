ffb1734a23e1d08d3d245b8fbe30d63b55800a83
c7x43t <c7x43t@gmail.com>
2018-05-03 11:14:17 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -2,13 +2,10 @@ let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
-
-const gestureZone = document.getElementById('modalContent');
-
+const gestureZone = document;//.getElementById('modalContent');
 gestureZone.addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
-	handleGesture(event);
 }, false);
 
 gestureZone.addEventListener('touchend', function(event) {
@@ -16,25 +13,31 @@ gestureZone.addEventListener('touchend', function(event) {
     touchendY = event.changedTouches[0].screenY;
     handleGesture(event);
 }, false);
-
-function handleGesture() {
-    if (touchendX < touchstartX) {
+function scalarProduct(a,b){
+	return a.reduce((acc,e,i)=>acc+e*b[i],0);
+}
+function vecLength(v){
+	return Math.sqrt(v.reduce((acc,e)=>acc+e**2,0))
+}
+function vecAngle(a,b){
+	return scalarProduct(a,b)/(vecLength(a)*vecLength(b));
+}
+function handleGesture() {	
+	vec=[touchendX-touchstartX,touchendY-touchstartY];
+	var angle=[vecAngle(vec,[1,0]),vecAngle(vec,[0,1])];
+	console.log(angle);	
+    if (angle[0]<=-0.5) {
         console.log('Swiped left');
-    }
-    
-    if (touchendX > touchstartX) {
+    }else if (angle[0]>=0.5) {
         console.log('Swiped right');
     }
     
-    if (touchendY < touchstartY) {
+    if (angle[1]<=-0.5) {
         console.log('Swiped up');
-    }
-    
-    if (touchendY > touchstartY) {
+    }else if (angle[1]>=0.5) {
        console.log('Swiped down');
     }
-    
-    if (touchendY === touchstartY) {
+    if (angle[0]===NaN&&angle[1]===NaN) {
        console.log('Tap');
     }
 }
