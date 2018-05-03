2825b07d2e3107381e144c6dfbc8e2aa11c4b599
c7x43t <c7x43t@gmail.com>
2018-05-03 14:09:32 +0200

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,22 +1,56 @@
 // for Promise polyfill see https://cdn.jsdelivr.net/npm/es6-promise/dist/es6-promise.js 
 // https://developers.google.com/web/fundamentals/primers/promises
 // https://github.com/Reactive-Extensions/RxJS
+// https://highlandjs.org/
 function newStream(){
     let store={};
     let finite={}; // not yet implemented ~ once and more {fn.name:n}
     let error={}; // currently no direct access to error channel
+    let buffers={};
+    let transforms=[];
+    let retryCount=0;
+    let retryMax=0;
+    let filtered=false;
+    let transformers={
+        map: function(data,fn){
+            return fn(data);
+        },
+        filter: function(data,fn){
+            if(!fn(data)){
+                filtered=true;
+            }
+        },
+    }
     function stream(data){
+        // transform data
+        for(let transform of transforms){
+            data=transformers[transform.type](data,transform.fn);
+        }
+        // push data
         let fns=Object.keys(store);
-        for(var i=0;i<fns.length;i++){
-            try{
-                store[fns[i]](data);
-            }catch(err){ // propably wrong implementation
-                let errs=Object.keys(error);
-                for(var j=0;j<errs.length;j++){
-                    error[errs[j]](err);
+        // iterate functions in store
+        if(!filtered){
+            for(var i=0;i<fns.length;i++){
+                try{
+                    // execute functions in store
+                    store[fns[i]](data);
+                }catch(err){ // propably wrong implementation
+                    // retry if configured
+                    if(retryCount<retryMax){
+                        i--;
+                        retryCount++;
+                        continue;
+                    }
+                    // push error to registered error channels
+                    let errs=Object.keys(error);
+                    for(var j=0;j<errs.length;j++){
+                        error[errs[j]](err);
+                    }
                 }
+                retryCount=0;
             }
         }
+        filtered=false;
     }
     stream.add=function(fn){
         store[fn.name]=fn;
@@ -34,6 +68,24 @@ function newStream(){
         delete error[fn.name];
         return stream;
     }
+    //mimic API
+    stream.subscribe=stream.add;
+    stream.retry=function(n){
+        retryMax=n;
+    }
+    stream.fromEvent = function(obj,name){
+        obj.addEventListener(name,stream)
+    }
+    stream.map=function(fn){
+        transforms.push({type:"map",fn:fn})
+    }
+    stream.filter=function(fn){
+        transforms.push({type:"filter",fn:fn})
+    }
+    stream.observe=function(observable){
+        observable.subscribe(stream);
+    }
+
     return stream;
 }
 //Test
@@ -51,3 +103,18 @@ let stream=newStream()
 
 stream(1);
 stream(2);
+
+
+// Iterator + imagine backpressure:
+/*
+range(0, 1000, 1)
+.map(function (n) {
+    return n * 2;
+})
+.filter(function (n) {
+    return n % 3 !== 0;
+})
+.reduce(function (a, b) {
+    return a + b;
+})
+*/
