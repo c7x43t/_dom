b4ff8a3df0f7f4026d213357fc5662c21a0047af
c7x43t <c7x43t@gmail.com>
2018-05-04 19:58:54 +0200

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,52 +1,62 @@
 function newPromise(fn){
+    const PROMISE_STATUS="[[PromiseStatus]]";
+    const PROMISE_VALUE="[[PromiseValue]]";
+	const PENDING="pending";
+	const RESOLVED="resolved";
+	const REJECTED="rejected";
     let fnThen=undefined;
     let fnCatch=undefined;
     let fnFinally=undefined;
     let reason=undefined;
     let self=this;
     function resolve(value){
-        promise["[[PromiseStatus]]"]="resolved";
-        promise["[[PromiseValue]]"]=value;
+        promise[PROMISE_STATUS]=RESOLVED;
+        promise[PROMISE_VALUE]=value;
         if(fnThen){
             fnThen(value);
             initialize();
         }
-        if(fnFinally) fnFinally();
+        if(fnFinally) fnFinally(); //?
     }
     function reject(reason){
         self.reason=reason;
-        promise["[[PromiseStatus]]"]="rejected";
-        promise["[[PromiseValue]]"]=reason;
+        promise[PROMISE_STATUS]=REJECTED;
+        promise[PROMISE_VALUE]=reason;
         if(fnCatch){
             fnCatch(reason);
             initialize();
         }else{
             throw reason;
         }
-        if(fnFinally) fnFinally();    
+        if(fnFinally) fnFinally(); //?
     }
     function initialize(){
-        promise["[[PromiseStatus]]"]="pending";
-        promise["[[PromiseValue]]"]=undefined;
+        promise[PROMISE_STATUS]=PENDING;
+        promise[PROMISE_VALUE]=undefined;
+		fnThen=undefined;
+		fnCatch=undefined;
+		fnFinally=undefined;
+		reason=undefined;
     }
     let promise={};
     initialize();
     promise.then=function(fn){
-        if(promise["[[PromiseStatus]]"]==="resolved"){
-            fn(promise["[[PromiseStatus]]"]);
+        if(promise[PROMISE_STATUS]===RESOLVED){
+            fn(promise[PROMISE_STATUS]);
+			initialize();
         }
         fnThen=fn;
         return promise;
     }
     promise.finally=function(fn){
-        if(promise["[[PromiseStatus]]"]!=="pending"){
+        if(promise[PROMISE_STATUS]!==PENDING){
             fn();
         }
         fnFinally=fn;
         // what to return?
     }
     promise.catch=function(fn){
-        if(promise["[[PromiseStatus]]"]==="rejected"){
+        if(promise[PROMISE_STATUS]===REJECTED){
             fn(reason);
             throw reason;
         }
