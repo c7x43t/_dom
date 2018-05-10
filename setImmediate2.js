dcfdf946091b4284607c32869a08ace8dcf63ce8
c7x43t <c7x43t@gmail.com>
2018-05-11 00:13:24 +0200

    Update setImmediate2.js

--- setImmediate2.js
+++ setImmediate2.js
@@ -1,32 +1,36 @@
 // very fast minimal implementation of setImmediate polyfill using MutationObserver (4-10x faster than messageChannel)
 // lacks currently: returning proper handle => window.clearImmediate
-// fix: missing arguments: setImmediate(fn,...args)
 // it works as expected otherwise, executing passed function async and in order
 var setImmediate=(function setImmediateFactory(){
     let resolver=function(){};
+    let args=[];
     let nextHandle = 1;
     let executing = false;
     let stack=[];
     let observer = new (MutationObserver || WebKitMutationObserver)(function(){ 
-        resolver();
+        resolver(...args);
         if(stack.length>0){
-            run(stack.shift());
+            let item=stack.shift();
+            run(item[0],item[1]);
         }else{
             executing=false;
         }
     });
     let observedElement = document.createElement('div');
     observer.observe(observedElement, { attributes: true });
-    function run(fn){
+    function run(fn,_args){
         resolver = fn;
+        args=_args;
         observedElement.setAttribute('a', '');
     }
-    return function setImmediate(fn){
+    return function setImmediate(){
+        let fn=arguments[0];
+        let _args=Array.apply(null, Array(arguments.length-1)).map((e,i)=>arguments[i+1]);
         if(executing){
-            stack.push(fn);
+            stack.push([fn,args]);
         }else{
             executing=true;
-            run(fn);
+            run(fn,_args);
         }
         return nextHandle++;
     }
