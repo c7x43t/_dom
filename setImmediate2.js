6dd41dbe8e0354f7386a2755d3b56372de11bd54
c7x43t <c7x43t@gmail.com>
2018-05-11 20:52:49 +0200

    Update setImmediate2.js

--- setImmediate2.js
+++ setImmediate2.js
@@ -1,37 +1,69 @@
 // very fast minimal implementation of setImmediate polyfill using MutationObserver (4-10x faster than messageChannel)
-// lacks currently: returning proper handle => window.clearImmediate
-// it works as expected otherwise, executing passed function async and in order
-var setImmediate=(function setImmediateFactory(){
-    let resolver=function(){};
-    let args=[];
-    let nextHandle = 1;
-    let executing = false;
-    let stack=[];
-    let observer = new (MutationObserver || WebKitMutationObserver)(function(){ 
-        resolver(...args);
-        if(stack.length>0){
-            let item=stack.shift();
-            run(item[0],item[1]);
+window.setImmediate=(function(){
+	var stack=[];
+	var handle=1;
+	var executing=false;
+    var ids={};
+    
+    function fastApply(fn,args){
+        var fastSpread={
+            0:function(fn){
+                fn();
+            },
+            1:function(fn,args){
+                fn(args[0]);
+            },
+            2:function(fn,args){
+                fn(args[0],args[1]);
+            },
+            3:function(fn,args){
+                fn(args[0],args[1],args[2]);
+            },
+            4:function(fn,args){
+                fn(args[0],args[1],args[2],args[3]);
+            },
+            5:function(fn,args){
+                fn(args[0],args[1],args[2],args[3],args[4]);
+            }
+        };
+        var len=args.length;
+        if(len<=5){
+            fastSpread[len](fn,args);
         }else{
-            executing=false;
+            fn.apply(undefined, args);
         }
+    }
+	var observer = new MutationObserver(function runner(){
+		var item=stack.shift();
+		if(ids.hasOwnProperty(item[2])){
+			window.clearImmediate(item[2]);
+            fastApply(item[0],item[1]);
+		}
+		if(stack.length>0){
+			runner();
+		}else{
+			executing=false;
+		}
     });
-    let observedElement = document.createElement('div');
+	var observedElement = document.createElement('div');
     observer.observe(observedElement, { attributes: true });
-    function run(fn,_args){
-        resolver = fn;
-        args=_args;
+    function run(){
         observedElement.setAttribute('a', '');
     }
-    return function setImmediate(){
-        let fn=arguments[0];
-        let _args=Array.apply(null, Array(arguments.length-1)).map((e,i)=>arguments[i+1]);
-        if(executing){
-            stack.push([fn,args]);
-        }else{
+	window.clearImmediate=function(id){
+		delete ids[id];
+    };
+	return function(){
+		var fn=arguments[0];
+        var args=[];
+        for(var i=1;i<arguments.length;i++) args.push(arguments[i]);
+		stack.push([fn,args,handle]);
+		ids[handle]=true;
+
+		if(!executing){
             executing=true;
-            run(fn,_args);
+            run();
         }
-        return nextHandle++;
-    }
+		return handle++;
+	};
 })();
