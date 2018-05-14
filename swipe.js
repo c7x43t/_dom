51ed3cd4f6aa874d408a40a341ca1fe234dabc15
c7x43t <c7x43t@gmail.com>
2018-05-14 13:14:22 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -25,18 +25,8 @@ function handleGesture(e) {
     let y = touchendY - touchstartY;
     let xy = Math.abs(x / y);
     let yx = Math.abs(y / x);
-    let detail = {
-        left: false,
-        right: false,
-        top: false,
-        topLeft:false,
-        topRight:false,
-        bottom: false,
-        bottomLeft:false,
-        bottomRight:false,
-        tap: false
-    };
-    let left,right,top,bottom;
+    let detail = {};
+    let left, right, top, bottom;
     if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
         if (yx <= limit) {
             if (x < 0) {
@@ -53,24 +43,24 @@ function handleGesture(e) {
             }
         }
     }
-    if(top&&left){
-        detail.topLeft=true;
-    }else if(top&&right){
-        detail.topRight=true;
-    }else if(bottom&&left){
-        detail.bottomLeft=true;
-    }else if(bottom&&right){
-        detail.bottomRight=true;
-    }else if(left){
-        detail.left=true;
-    }else if(right){
-        detail.right=true;
-    }else if(bottom){
-        detail.bottom=true;
-    }else if(top){
-        detail.top=true;
-    }else{
-        detail.tap=true;
+    if (top && left) {
+        detail.topLeft = true;
+    } else if (top && right) {
+        detail.topRight = true;
+    } else if (bottom && left) {
+        detail.bottomLeft = true;
+    } else if (bottom && right) {
+        detail.bottomRight = true;
+    } else if (left) {
+        detail.left = true;
+    } else if (right) {
+        detail.right = true;
+    } else if (bottom) {
+        detail.bottom = true;
+    } else if (top) {
+        detail.top = true;
+    } else {
+        detail.tap = true;
     }
     document.dispatchEvent(new CustomEvent('swipe', {
         bubbles: true,
