eece8d072414322a6ee0b085102c284359fb342c
c7x43t <c7x43t@gmail.com>
2018-05-14 12:59:48 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,14 +1,14 @@
 // FROM: https://gist.github.com/SleepWalker/da5636b1abcbaff48c4d
 // GOAL: https://www.npmjs.com/package/pure-swipe-js
-const limit = Math.cos(Math.PI * (3 / 8));
 let pageWidth = window.innerWidth || document.body.clientWidth;
-let treshold = Math.floor(0.01 * (pageWidth));
+let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
 let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
-const gestureZone = document; //.getElementById('modalContent');
 
+const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+const gestureZone = document;
 gestureZone.addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
@@ -20,34 +20,30 @@ gestureZone.addEventListener('touchend', function(event) {
     handleGesture(event);
 }, false);
 
-function scalarProduct(a, b) {
-    return a.reduce((acc, e, i) => acc + e * b[i], 0);
-}
-
-function vecLength(v) {
-    return Math.sqrt(v.reduce((acc, e) => acc + e ** 2, 0))
-}
-
-function vecAngle(a, b) {
-    return scalarProduct(a, b) / (vecLength(a) * vecLength(b));
-}
-
-function handleGesture() {
-    const vec = [touchendX - touchstartX, touchendY - touchstartY];
-    const angle = [vecAngle(vec, [1, 0]), vecAngle(vec, [0, 1])];
-    if (!(isNaN(angle[0]) && isNaN(angle[1]) || Math.abs(vec[0]) < treshold && Math.abs(vec[1]) < treshold)) {
-        if (angle[0] <= -limit) {
-            console.log('Swiped left');
-        } else if (angle[0] >= limit) {
-            console.log('Swiped right');
+function handleGesture(e) {
+    let x = touchendX - touchstartX;
+    let y = touchendY - touchstartY;
+    let xy = Math.abs(x / y);
+    let yx = Math.abs(y / x);
+let detail={left:false,right:false,top:false,bottom:false,tap:false};    
+if (Math.abs(x) > treshold || Math.abs(y) > treshold) {		
+	
+        if (yx <= limit) {
+            if (x < 0) {
+                detail.left=true;
+            } else {
+                detail.right=true;
+            }
         }
-
-        if (angle[1] <= -limit) {
-            console.log('Swiped up');
-        } else if (angle[1] >= limit) {
-            console.log('Swiped down');
+        if (xy <= limit) {
+            if (y < 0) {
+                detail.top=true;
+            } else {
+                detail.bottom=true;
+            }
         }
     } else {
-        console.log('Tap');
+        detail.tap=true;
     }
+	document.dispatchEvent(new CustomEvent('swipe', { bubbles: true, detail ,a:3}));
 }
