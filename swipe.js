c59bcda3724807b8b4d9d6e90ed76a6b44d4915e
c7x43t <c7x43t@gmail.com>
2018-05-14 13:01:23 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,7 +1,7 @@
 // FROM: https://gist.github.com/SleepWalker/da5636b1abcbaff48c4d
 // GOAL: https://www.npmjs.com/package/pure-swipe-js
 let pageWidth = window.innerWidth || document.body.clientWidth;
-let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
+let treshold = Math.max(1, Math.floor(0.01 * (pageWidth)));
 let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
@@ -25,25 +25,34 @@ function handleGesture(e) {
     let y = touchendY - touchstartY;
     let xy = Math.abs(x / y);
     let yx = Math.abs(y / x);
-let detail={left:false,right:false,top:false,bottom:false,tap:false};    
-if (Math.abs(x) > treshold || Math.abs(y) > treshold) {		
-	
+    let detail = {
+        left: false,
+        right: false,
+        top: false,
+        bottom: false,
+        tap: false
+    };
+    if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+
         if (yx <= limit) {
             if (x < 0) {
-                detail.left=true;
+                detail.left = true;
             } else {
-                detail.right=true;
+                detail.right = true;
             }
         }
         if (xy <= limit) {
             if (y < 0) {
-                detail.top=true;
+                detail.top = true;
             } else {
-                detail.bottom=true;
+                detail.bottom = true;
             }
         }
     } else {
-        detail.tap=true;
+        detail.tap = true;
     }
-	document.dispatchEvent(new CustomEvent('swipe', { bubbles: true, detail ,a:3}));
+    document.dispatchEvent(new CustomEvent('swipe', {
+        bubbles: true,
+        detail
+    }));
 }
