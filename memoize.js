b955558577ba41db9f1cada6fc97379cd9503532
c7x43t <c7x43t@gmail.com>
2018-05-17 11:50:23 +0200

    Update memoize.js

--- memoize.js
+++ memoize.js
@@ -1,12 +1,12 @@
-function memoize(fn){
-	let map={};
-	return function(e){
-    if(Reflect.get(map,e)){ // from cache
-        return map[e];
-    }else{ // from function
-        let val=fn(e);
-        map[e]=val;
-        return val;
-    }
+function memoize(fn) {
+    let map = {};
+    return function(e) {
+        if (Reflect.get(map, e)) { // from cache
+            return map[e];
+        } else { // from function
+            let val = fn(e);
+            map[e] = val;
+            return val;
+        }
     }
 }
