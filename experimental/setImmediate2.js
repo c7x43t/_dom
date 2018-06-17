23c7b89aead4c0f4dc83b2639c0cc2edacd0725c
c7x43t <c7x43t@gmail.com>
2018-06-17 20:06:02 +0200

    Update setImmediate2.js

--- experimental/setImmediate2.js
+++ experimental/setImmediate2.js
@@ -8,31 +8,34 @@ window.setImmediate = (function() {
     var ids = {};
 
     function fastApply(fn, args) {
-        var fastSpread = {
-            0: function(fn) {
+        var fastSpread = [
+            function(fn) {
                 fn();
             },
-            1: function(fn, args) {
+            function(fn, args) {
                 fn(args[0]);
             },
-            2: function(fn, args) {
+            function(fn, args) {
                 fn(args[0], args[1]);
             },
-            3: function(fn, args) {
+            function(fn, args) {
                 fn(args[0], args[1], args[2]);
             },
-            4: function(fn, args) {
+            function(fn, args) {
                 fn(args[0], args[1], args[2], args[3]);
             },
-            5: function(fn, args) {
+            function(fn, args) {
                 fn(args[0], args[1], args[2], args[3], args[4]);
+            },
+            function(fn, args) {
+                fn(args[0], args[1], args[2], args[3], args[4], args[5]);
             }
-        };
+        ];
         var len = args.length;
-        if (len <= 5) {
+        if (len <= 6) {
             fastSpread[len](fn, args);
         } else {
-            fn.apply(undefined, args);
+            fn.apply(null, args);
         }
     }
 
