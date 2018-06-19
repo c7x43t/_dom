ffc1bbd59892e2e90c7be906fd6b7729dafb6bb5
c7x43t <c7x43t@gmail.com>
2018-06-19 13:11:28 +0200

    Update fastApply.js

--- modules/fastApply.js
+++ modules/fastApply.js
@@ -2,31 +2,31 @@
 function fastApply(fn, args) {
     var fastSpread = [
         function(fn) {
-            fn();
+            return fn();
         },
         function(fn, args) {
-            fn(args[0]);
+            return fn(args[0]);
         },
         function(fn, args) {
-            fn(args[0], args[1]);
+            return fn(args[0], args[1]);
         },
         function(fn, args) {
-            fn(args[0], args[1], args[2]);
+            return fn(args[0], args[1], args[2]);
         },
         function(fn, args) {
-            fn(args[0], args[1], args[2], args[3]);
+            return fn(args[0], args[1], args[2], args[3]);
         },
         function(fn, args) {
-            fn(args[0], args[1], args[2], args[3], args[4]);
+            return fn(args[0], args[1], args[2], args[3], args[4]);
         },
         function(fn, args) {
-            fn(args[0], args[1], args[2], args[3], args[4], args[5]);
+            return fn(args[0], args[1], args[2], args[3], args[4], args[5]);
         }
     ];
     var len = args.length;
     if (len <= 6) {
-        fastSpread[len](fn, args);
+        return fastSpread[len](fn, args);
     } else {
-        fn.apply(null, args);
+        return fn.apply(null, args);
     }
 }
