149be5b0b40ec2af6f1fb4fed347df5f0dcd6887
c7x43t <c7x43t@gmail.com>
2018-06-20 13:04:07 +0200

    Update animationFrame.js

--- modules/animationFrame.js
+++ modules/animationFrame.js
@@ -4,41 +4,44 @@
 // usage: context.animationFrame = (new animationFrame).exec; // only a single object is necessary globally
 // usage: animationFrame(myFunc,args);
 // named functions will be processed faster
-function _animationFrame(fn) {
-    var tolerance = 0.1;
-    var fps = 60;
-    var delay = 1e3 / fps;
-    var lastTimeout = 0;
-    var lastIteration = 0;
-    var args;
+global.animationFrame = (function() {
+    function _animationFrame(fn) {
+        var tolerance = 0.1;
+        var fps = 60;
+        var delay = 1e3 / fps;
+        var lastTimeout = 0;
+        var lastIteration = 0;
+        var args;
 
-    function iteration() {
-        lastIteration = +new Date;
-        fastApply(fn, args);
-    }
-
-    function run() {
-        var thisIteration = +new Date;
-        args = arguments;
-        if (thisIteration - lastIteration > delay - tolerance) {
-            clearTimeout(lastTimeout);
-            iteration();
-        } else {
-            lastTimeout = setTimeout(iteration, delay % (thisIteration - lastIteration))
+        function iteration() {
+            lastIteration = +new Date;
+            fastApply(fn, args);
         }
-    }
-    this.run = run;
-}
 
-function animationFrame() {
-    store = {};
-
-    function exec(fn, args) {
-        var name = fn.name !== "" ? fn.name : fn.toString();
-        if (!store.hasOwnProperty(name)) {
-            store[name] = new _animationFrame(fn);
+        function run() {
+            var thisIteration = +new Date;
+            args = arguments;
+            if (thisIteration - lastIteration > delay - tolerance) {
+                clearTimeout(lastTimeout);
+                iteration();
+            } else {
+                lastTimeout = setTimeout(iteration, delay % (thisIteration - lastIteration))
+            }
         }
-        store[name].run(args);
+        this.run = run;
     }
-    this.exec = exec;
-}
+
+    function animationFrame() {
+        store = {};
+
+        function exec(fn, args) {
+            var name = fn.name !== "" ? fn.name : fn.toString();
+            if (!store.hasOwnProperty(name)) {
+                store[name] = new _animationFrame(fn);
+            }
+            store[name].run(args);
+        }
+        this.exec = exec;
+    }
+    return (new animationFrame).exec;
+}());
