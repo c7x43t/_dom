2e1211e4aba55b6b04eb2023c81c3ade8ff78bca
c7x43t <c7x43t@gmail.com>
2018-06-27 11:39:39 +0200

    Update setImmediate2.js

--- experimental/setImmediate2.js
+++ experimental/setImmediate2.js
@@ -1,43 +1,46 @@
 // very fast, minimal implementation of setImmediate polyfill using MutationObserver (4-10x faster than messageChannel)
 // https://github.com/Octane/setImmediate/blob/master/setimmediate.js
 // https://github.com/YuzuJS/setImmediate 
-window.setImmediate = (function() {
+function fastApply(fn, args) {
+    var fastSpread = [
+        function(fn) {
+            fn();
+        },
+        function(fn, args) {
+            fn(args[0]);
+        },
+        function(fn, args) {
+            fn(args[0], args[1]);
+        },
+        function(fn, args) {
+            fn(args[0], args[1], args[2]);
+        },
+        function(fn, args) {
+            fn(args[0], args[1], args[2], args[3]);
+        },
+        function(fn, args) {
+            fn(args[0], args[1], args[2], args[3], args[4]);
+        },
+        function(fn, args) {
+            fn(args[0], args[1], args[2], args[3], args[4], args[5]);
+        }
+    ];
+    var len = args.length;
+    if (len <= 6) {
+        fastSpread[len](fn, args);
+    } else {
+        fn.apply(null, args);
+    }
+}
+
+
+(function() {
     var stack = [];
     var handle = 1;
     var executing = false;
     var ids = {};
-
-    function fastApply(fn, args) {
-        var fastSpread = [
-            function(fn) {
-                fn();
-            },
-            function(fn, args) {
-                fn(args[0]);
-            },
-            function(fn, args) {
-                fn(args[0], args[1]);
-            },
-            function(fn, args) {
-                fn(args[0], args[1], args[2]);
-            },
-            function(fn, args) {
-                fn(args[0], args[1], args[2], args[3]);
-            },
-            function(fn, args) {
-                fn(args[0], args[1], args[2], args[3], args[4]);
-            },
-            function(fn, args) {
-                fn(args[0], args[1], args[2], args[3], args[4], args[5]);
-            }
-        ];
-        var len = args.length;
-        if (len <= 6) {
-            fastSpread[len](fn, args);
-        } else {
-            fn.apply(null, args);
-        }
-    }
+    var shift = [].shift;
+    var implementation;
 
     function runner() {
         var item = stack.shift();
@@ -51,32 +54,48 @@ window.setImmediate = (function() {
             executing = false;
         }
     }
-    var observer = new MutationObserver(runner);
-    var observedElement = document.createElement('div');
-    observer.observe(observedElement, {
-        attributes: true
-    });
+    if (MutationObserver) {
+        var observer = new MutationObserver(runner);
+        var observedElement = document.createElement('div');
+        observer.observe(observedElement, {
+            attributes: true
+        });
+        implementation = 0;
+    } else if (MessageChannel) {
+        var channel = new MessageChannel();
+        channel.port1.onmessage = runner;
+        channel.port1.start();
+        channel.port2.start();
+        implementation = 1;
+    }
 
-    function run() {
+    var run = [function run() {
         executing = true;
         observedElement.setAttribute('a', '');
-    }
+    }, function run() {
+        executing = true;
+        channel.port2.postMessage('');
+    }]
     window.clearImmediate = function(id) {
         ids[id] = null;
         delete ids[id];
     };
-    return function() {
-        var fn = arguments[0];
-        var args = [];
-        for (var i = 1; i < arguments.length; i++) args.push(arguments[i]);
-        /*if (stack.length >= 30) {
-            runner();
-        }*/
-        stack.push([fn, args, handle]);
-        ids[handle] = true;
-        if (!executing) {
-            run();
+    try {
+        if (!window.setImmediate) {
+            window.setImmediate = function() {
+                arguments.shift = shift;
+                var fn = arguments.shift();
+                stack.push([fn, arguments, handle]);
+                //for (var i = 1; i < arguments.length; i++) args.push(arguments[i]);
+                /*if (stack.length >= 30) {
+                	runner();
+                }*/
+                ids[handle] = true;
+                if (!executing) {
+                    run[implementation]();
+                }
+                return handle++;
+            };
         }
-        return handle++;
-    };
+    } catch (e) {}
 })();
