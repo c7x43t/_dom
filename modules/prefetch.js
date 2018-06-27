da806f86ce52625d08362a58bd54569f140cd1ea
c7x43t <c7x43t@gmail.com>
2018-06-28 00:49:37 +0200

    Create prefetch.js

---
+++ modules/prefetch.js
@@ -0,0 +1,88 @@
+(function(scope){
+	var options={
+		delay:250
+	};
+	var prefetchID="prefetch"+Date.now();
+	var store={
+		prefetched:{},
+		loading:{}
+	};
+	scope[prefetchID]={
+		fireEvent:function(){
+			store.loaded=true;
+			handlePageload();
+			if(store.static){
+				store.prefetched[store.target.href]=true;
+			}
+			var e=document.getElementById(prefetchID);
+			e.parentElement.removeChild(e);
+		}
+	};
+	var links=document.querySelectorAll("a");
+	Array.apply(null, links).map(registerLinkListeners);
+	function findNestedLink(element){
+		try{
+			return element.tagName==="A"?element:findNestedLink(element.parentElement);
+		}catch(e){}
+	}
+	function linkMouseDownListener(event){
+		if(event.button===0){
+			//console.log(event);
+			if(store.prefetched[store.url]){
+				store.loaded=true;
+			}else if(!store.loading[store.url]){
+				prefetchUrl(store.url);
+			}	
+		}
+	}
+	function prefetchUrl(url){
+		store.loading[url]=true;
+		let node = document.createElement('div');
+		node.innerHTML = `<link id="${prefetchID}" rel=prefetch href="${url}" onload="${prefetchID}.fireEvent()" />`;
+		document.querySelector('head').appendChild(node.childNodes[0]);
+	}
+	function linkMouseClickListener(event){
+		event.preventDefault();
+	}
+	function linkMouseEnterListener(event){
+		store.target=findNestedLink(event.target);
+		store.url=store.target.href;
+		store.loaded=false;
+		store.static=true;
+		store.mouse=false;
+		if(options.delay>0){
+			setTimeout(function(){
+				if(store.prefetched[store.url]){
+					store.loaded=true;
+				}else if(!store.loading[store.url]&&store.static){
+					prefetchUrl(store.url);
+				}
+			},options.delay);
+		}
+	}
+	function linkMouseOutListener(event){
+		store.static=false;
+	}
+	function linkMouseUpListener(event){
+		if(event.button===0){
+			store.mouse=true;
+			handlePageload();
+		}	
+	}
+	function registerLinkListeners(element){
+		element.addEventListener("mouseenter",linkMouseEnterListener);
+		element.addEventListener("mouseout",linkMouseOutListener);
+		element.addEventListener("mousedown",linkMouseDownListener);
+		element.addEventListener("mouseup",linkMouseUpListener);
+		element.addEventListener("click",linkMouseClickListener);
+	}
+	function handlePageload(){
+		//console.log({context:"handlePageload",store:store});
+		if((store.loaded||store.prefetched[store.target.href])&&store.static&&store.mouse){
+			navigateToTarget();
+		}
+	}
+	function navigateToTarget(){
+		scope.location.href = store.target.href;
+	}
+}(window));
