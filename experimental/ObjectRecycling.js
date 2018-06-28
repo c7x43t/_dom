b009a263f7e8dddf9a4f1fe05f34871f51327c8f
c7x43t <c7x43t@gmail.com>
2018-06-28 20:44:30 +0200

    Update ObjectRecycling.js

--- experimental/ObjectRecycling.js
+++ experimental/ObjectRecycling.js
@@ -10,25 +10,50 @@ function Recyclable(keys) {
         strArgs += key + ",";
     }
     strArgs = strArgs.substring(0, strArgs.length - 1);
-    var recyclable = new Function("", `
-        Object.defineProperties(this, {
-            recycle: {
-                value: function() {
-                    $ {
-                        strRecycle
-                    }
-                }
-            },
-            init: {
-                value: function($ {
-                    strArgs
-                }) {
-                    $ {
-                        strInit
-                    }
+    var recyclable = new Function("", `Object.defineProperties(this,{
+		recycle:{
+			value:function(){${strRecycle}}
+		},
+		init:{
+			value:function(${strArgs}){${strInit}}
+		}
+	})`)
+    return recyclable;
+}
+
+function objectPool(keys) {
+    if (!(keys instanceof Array)) keys = Object.keys(keys);
+    var recyclable = Recyclable(keys);
+    this.freed = [];
+    this.allocated = 0;
+    Object.defineProperties(this, {
+        allocate: {
+            value: function() {
+                if (this.freed.length > 0) {
+                    return this.freed.pop();
+                } else {
+                    this.allocated++;
+                    return new recyclable();
                 }
             }
-        })
-    `);
-    return recyclable;
+        },
+        free: {
+            value: function(n) {
+                var length = this.freed.length;
+                var l = length - n;
+                var newL = l < 0 ? 0 : l;
+                this.freed.length = newL;
+                return length - newL;
+            }
+        },
+        preAllocate: {
+            value: function(n) {
+                var diff = n - this.allocated;
+                this.allocated += diff;
+                for (var i = 0; i < diff; i++) {
+                    this.freed.push(new recyclable());
+                }
+            }
+        },
+    });
 }
