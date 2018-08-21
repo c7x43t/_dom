ee720e67578e5e3efafbbac270deef7e21da18f5
c7x43t <c7x43t@gmail.com>
2018-08-21 13:22:14 +0200

    Update findElementsByContent.js

--- findElementsByContent.js
+++ findElementsByContent.js
@@ -3,7 +3,7 @@ function findElementsByContent(document,str,selector){
   if(selector===undefined) selector="*";
   var els = typeof selector==="string"?document.querySelectorAll(selector):Array.from(elements);
   for(var i=0; i<els.length; i++){
-	var text=els[i].text
+	var text=els[i].innerText;
       if(text&&text.includes(str)){
             results.push(els[i]);
       }
