e68f4e59a2eab5af2537f88711ae6752f7821b63
c7x43t <c7x43t@gmail.com>
2018-10-02 12:03:31 +0300

    Create parseFunction.js

---
+++ parseFunction.js
@@ -0,0 +1,13 @@
+// parses a function returing an object {args: array of arguments, body: function body as string}
+function parseFunction(func){
+    var regParseFunction=/\(([\s\S]*?)\) *(?:=>)? *{([\s\S]*?)}/;
+    var parsedFunction=regParseFunction.exec(func.toString());
+    if(parsedFunction){
+        var args=parsedFunction[1];
+        var body=parsedFunction[2];
+    }
+    return typeof args === "string" && typeof body === "string" ? {
+        args: args.split(","),
+        body: body
+    } : null;
+}
