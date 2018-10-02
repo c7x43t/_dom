15e2ecbe104e37a6c98a1aa3cc74641c51bb9883
c7x43t <c7x43t@gmail.com>
2018-10-02 12:04:03 +0300

    Update parseFunction.js

--- parseFunction.js
+++ parseFunction.js
@@ -1,4 +1,5 @@
 // parses a function returing an object {args: array of arguments, body: function body as string}
+// if parsing fails null is returned
 function parseFunction(func){
     var regParseFunction=/\(([\s\S]*?)\) *(?:=>)? *{([\s\S]*?)}/;
     var parsedFunction=regParseFunction.exec(func.toString());
